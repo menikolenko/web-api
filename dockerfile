4108059fb0c1ba8c36cd54850c790ab3b0e4e165
menikolenko <menikolenko@gmail.com>
2018-11-02 11:26:40 -0700

    change code

--- dockerfile
+++ dockerfile
@@ -1,6 +1,6 @@
 FROM ubuntu
 MAINTAINER menikolenko <menikolenko@gmail.com>
-RUN apt-get update && apt-get install -y netcat
+RUN apt-get update && apt-get install -y netcat postgresql-client
 WORKDIR /app
 COPY ./web-api.sh /app
 RUN chmod +x ./web-api.sh
--- web-api.sh
+++ web-api.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-make_response () {
-echo -en "HTTP/1.1 200 OK\r\nConnection: keep-alive\r\n\r\n
-Hello from api!
-$(PGPASSWORD=qwerty psql -U postgres -h backend -p 5432 -c '\l')"
-}
-while make_response | nc -l "${WEB_API_PORT:-7001}"; do
-echo "================================================"
+#make_response () {
+#echo -en "HTTP/1.1 200 OK\r\nConnection: keep-alive\r\n\r\n
+#Hello from api!
+#$(PGPASSWORD=qwerty psql -U postgres -h 172.17.0.1 -p 5432 -c '\l')"
+#}
+#while make_response | nc -l "${WEB_API_PORT:-7001}"; do
+#echo "================================================"
+#done
+
+
+while true;
+
+do
+
+  echo -en "HTTP/1.1 200 OK\r\nConnection: keep-alive\r\n\r\n
+  Hello from api!
+  $(PGPASSWORD=qwerty psql -U postgres -h backend -p 5432 -c '\l')" | nc -l -p 7001 -q 1
+
 done
